64c4cdc74cf882a87ecca98563f95b4194c707fe
JannyDream <haruto.yamamoto.king@gmail.com>
2023-09-06 14:26:29 +0200

    fix: update carttest to cartmodeltest

--- vita-meals-backend/tests/Unit/CartModelTest.php
+++ vita-meals-backend/tests/Unit/CartModelTest.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class CartTest extends TestCase
+class CartModelTest extends TestCase
 {
     use RefreshDatabase;
 
